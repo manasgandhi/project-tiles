c14e7f446a2af708cff5cfee890e1513efd137dc
manasgandhi <157616964+manasgandhi@users.noreply.github.com>
2024-05-23 23:16:15 +0530

    Add files via upload

---
+++ page2/index.js
@@ -0,0 +1,17 @@
+var starting = document.querySelector(".easy");
+
+starting.addEventListener("click", function(){
+    document.location.href="../page3 - Easy/page3easy.html";
+})
+
+var starting = document.querySelector(".medium");
+
+starting.addEventListener("click", function(){
+    document.location.href="../page3 - Medium/page3medium.html";
+})
+
+var starting = document.querySelector(".hard");
+
+starting.addEventListener("click", function(){
+    document.location.href="../page3 - Hard/page3hard.html";
+})
